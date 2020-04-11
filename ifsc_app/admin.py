bab67b943e25bad58035b84aac830d6ac43c6f37
abhishek <mailabhipoear@gmail.com>
2020-04-11 22:24:17 +0530

    minor fix

--- ifsc_app/admin.py
+++ ifsc_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bank_Details
 
 # Register your models here.
+class BankAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Bank_Details, BankAdmin)
