2c89baa1d186a00e7edc697a59c65d3c0507c66a
abhishek <mailabhipoear@gmail.com>
2020-04-11 18:13:27 +0530

    minr changes

--- credicxo/credicxo/settings.py
+++ credicxo/credicxo/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5(7g^ih_urrkbindh^-%u54o60jsdgpb*nd7j!9y1xo^+c1rx8'
+SECRET_KEY = os.environ.get('BLOG_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bankdjango.herokuapp.com']
 
